59c55b144b1d547b0b2f9e8ea7c969b54b78e907
Mark T. Holder <mtholder@gmail.com>
2015-07-03 11:10:26 +0200

    dealing with name change of the example file

--- example/run.sh
+++ example/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../src/fastdate --method_nodeprior --tree_file ../example/node_prior.tre --prior_file ../example/node_prior.txt --out_file test2 --bd_mu 2 --bd_lambda 3 --max_age 100 --bd_rho 0.5 --show_tree --grid 100
+../src/fastdate --method_nodeprior --tree_file ../example/small.tre --prior_file ../example/node_prior.txt --out_file test2 --bd_mu 2 --bd_lambda 3 --max_age 100 --bd_rho 0.5 --show_tree --grid 100
--- example/slowdate-invoc.sh
+++ example/slowdate-invoc.sh
@@ -1,1 +1,1 @@
-../slowdate/slowdate --verbose --method_nodeprior --tree_file ../example/node_prior.tre --prior_file ../example/node_prior.txt --out_file test2 --bd_mu 2 --bd_lambda 3 --max_age 100 --bd_rho 0.5 --show_tree --grid 100
+../slowdate/slowdate --verbose --method_nodeprior --tree_file ../example/small.tre --prior_file ../example/node_prior.txt --out_file test2 --bd_mu 2 --bd_lambda 3 --max_age 100 --bd_rho 0.5 --show_tree --grid 100
