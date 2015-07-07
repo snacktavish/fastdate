57277a2822d28dae795b091e7dee747b5138939b
flouris <Tomas.Flouri@h-its.org>
2015-07-07 15:22:17 +0200

    updated license name-list on modified files

--- src/fastdate.c
+++ src/fastdate.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2015 Tomas Flouri
+    Copyright (C) 2015 Tomas Flouri, Emily Jane McTavish
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
